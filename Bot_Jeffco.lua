6dcae230ff2bb29ff6b90c6e2554693d9ff10d9e
Borsty <borstymail@googlemail.com>
2012-11-17 17:49:21 +0100

    Updated changelog and copyright entries

--- Bot_Jeffco.lua
+++ Bot_Jeffco.lua
@@ -5,6 +5,7 @@
 // A simple bot implementation for Natural Selection 2
 //
 // Copyright 2011 Colin Graf (colin.graf@sovereign-labs.com)
+// Copyright 2012 Sebastian J. (borstymail@googlemail.com)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,8 +18,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
-// Edited by Sebastian J. (borstymail@googlemail.com)
 //
 //=============================================================================
 
